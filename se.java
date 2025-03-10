4a0c9db0b53bff9b8fe465cba245a842c439c6be
varun420abc <vh6162588@gmail.com>
2025-03-10 12:44:53 +0530

    Update se.java

--- se.java
+++ se.java
@@ -2,6 +2,6 @@ class hello
 {
 	public static void main(String[] args)
 	{
-		System.out.println("hello worl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d");
+		System.out.println("hello worlghp");
 	}
 }
